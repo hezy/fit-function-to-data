1b240be1ed023a78d5134c4f19025102c530ad76
Hezy <hezy.amiel@gmail.com>
2019-05-04 01:34:56 +0300

    Update planck.py

--- planck.py
+++ planck.py
@@ -73,7 +73,7 @@ axs[0].set_ylabel("intensity ()")
 axs[1].grid(True)
 axs[1].set_title("Wien’s Law")
 axs[1].set_xlabel("Temperature (K)")
-axs[1].set_ylabel("$\lambda_{max} (nm)$")
+axaxs[1].set_ylabel(r'$\lambda_{max} (nm)$')(nm)$")
 axs[1].set_yscale('log')
 
 plt.show()
