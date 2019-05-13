f2f97ad7586a837887dd421b5ee9506ee4b5f6c3
Hezy <hezy.amiel@gmail.com>
2019-05-14 00:55:23 +0300

    Update voigt.py

--- voigt.py
+++ voigt.py
@@ -6,6 +6,7 @@ Created on Sat May 11 16:13:38 2019
 https://en.m.wikipedia.org/wiki/Voigt_profile
 http://journals.iucr.org/j/issues/1997/04/00/gl0484/gl0484.pdf
 http://journals.iucr.org/j/issues/2000/06/00/nt0146/nt0146.pdf
+https://www.onlinelibrary.wiley.com/doi/epdf/10.1002/sia.5521
 """
 
 import numpy as np
@@ -13,31 +14,32 @@ from scipy.special import wofz
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
+
 def lorentz(x, gamma):
-    #w = FWHM
-#    return 1. /(1. + np.square(x/w)) 
+    # Lorentz with max=1 and w=FWHM=1: return 1. /(1. + np.square(x/w)) 
+    # normolized Lorentz (integral = 1):
     return gamma/np.pi/(np.square(x) + np.square(gamma)) 
 
 
 def gauss(x, sigma):
-#    return np.exp(-np.log(2.)*np.square(x/w))
+    # Gauss with max=1 and w=FWHM=1: return np.exp(-np.log(2.)*np.square(x/w))
+    # normolized Gauss (integral = 1):
     return (np.exp(-np.square(x)/2/np.square(sigma)))/(sigma*np.sqrt(2*np.pi))
 
+
 def voigt(x, sigma, gamma, c):
-    """
-    for Lorentz sigma=0, gamma=1, c=1
-    for Gauss sigma=1, gamma=0, c=1
-    """
-#    s = np.sqrt(np.log(2))
-#    return c * np.real(wofz(s*(a*x/2 + 1j*b)))
+    # Voigt with max = 1 and FWHM=1: return c * np.real(wofz(np.sqrt(np.log(2))*(a*x/2 + 1j*b)))
+    # normolized Voigt (integral = 1):
     return c * np.real(wofz((x + 1j*gamma)/(sigma * np.sqrt(2)))) / (sigma * np.sqrt(2*np.pi))
+    # for Lorentz sigma=0, gamma=1, c=1
+    # for Gauss sigma=1, gamma=0, c=1
 
 
 x = np.arange (-12.0, 12.0 , 0.2)
 xfit = np.arange (-12.0, 12.0 , 0.01)
 
-yL = lorentz(x,1.)
-yG = gauss(x,1.)
+yL = lorentz(x,1.0)
+yG = gauss(x,1.0)
 yVtest = voigt(xfit, 1.0, 1.0, 1.0)
 
 popt_L, pcov_L = curve_fit(voigt, x, yL, p0=(1e-9, 1.0, 1.0), sigma=None)
