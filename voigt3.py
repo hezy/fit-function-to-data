3f4e9375e6faa7a6cc24cc7050c8facdf37ebc70
Hezy <hezy.amiel@gmail.com>
2019-05-25 20:03:32 +0300

    voigt3 voigt.ipynb

---
+++ voigt3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 15:53:03 2019
+
+@author: hezya
+"""
+
+import numpy as np
+from scipy.special import wofz
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from sympy import *
+
+x = Symbol('x')
+gamma = Symbol('gamma')
+L = Symbol('L')
+L = 1/(x**2 + gamma**2)
+L_norm = integrate(L, (x, -oo, oo))
