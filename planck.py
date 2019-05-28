f2f28d7e664aa378561c1019e1d5b512ee95a2f8
Hezy <hezy.amiel@gmail.com>
2019-05-28 13:43:19 +0300

    Update planck.py

--- planck.py
+++ planck.py
@@ -43,7 +43,8 @@ expected value is b = 2.8977729e−3 m*K
 def wien(x, a, b):
     return a + b/x
 
-# configuring the figure
+
+# preconfiguring the figure
 plt.close('all')
 plt.rcParams.update({'font.size': 12})
 fig, axs = plt.subplots(nrows=2, ncols=1, sharex=False, figsize=(8, 12))
@@ -51,13 +52,13 @@ plt.subplots_adjust(left=None, bottom=None, right=None, top=0.95, wspace=None, h
 fst = 16 #font size for title
 fsl = 14 #font size for axes labels
     
+
 # create Planck's curves for different temperatures
 x = np.arange(1, 10000 , 50) # wavelength (nm)
 xfit = np.arange(1, 10000, 1) # wavelength (nm)
-
-
 T = np.arange(500, 5000, 500) # temperature (K)
 Lmax = np.array([])
+
 for t in T:
     # creating Planck's curve for temperature = t (with some random noise)
     y = (planck(x,t)) *(1 + np.random.normal(0, 0.1, None)) 
@@ -65,7 +66,7 @@ for t in T:
     
     # fitting Planck's curve for t
     popt, pcov = curve_fit(planck, x, y, 2000)
-    print(popt, pcov)
+    print('fitted T =', popt, '+/-', pcov)
     yfit = planck(xfit, *popt)
     axs[0].plot(xfit, yfit, "-")
     
@@ -74,28 +75,29 @@ for t in T:
     Lmax = np.append(Lmax,np.argmax(yfit))
 
 
-# fitting Wien's curve with a 1/T function 
+# fitting Wien's curve with a + b/T 
 axs[1].plot(T, Lmax, "bo")
 Tfit = np.arange(300, 6000, 100) # temperature (K)
 popt, pcov = curve_fit(wien, T, Lmax)
+print('a,b = ')
 print(popt)
+print('Delta a,b = ')
 print(pcov)
 axs[1].plot(Tfit, wien(Tfit, *popt), "r-")
 
 
-# aranging graphs in figure
-# graph 1
+# aranging graph 1
 axs[0].grid(True)
 axs[0].set_title("Planck's curves", fontsize=fst)
 axs[0].set_xlabel("wavelength (nm)", fontsize=fsl)
-axs[0].set_xlim(0,10000)
+axs[0].set_xlim(0,4000)
 axs[0].set_ylabel(r'$spectral \: radiance \: (W/sr/m^3)$', fontsize=fsl)
-# graph 2
+# aranging graph 2
 axs[1].grid(True)
 axs[1].set_title("Wien’s Law", fontsize=fst)
 axs[1].set_xlabel("Temperature (K)", fontsize=fsl)
 axs[1].set_xlim(1,6000)
 axs[1].set_ylabel(r'$\lambda_{max} \: (nm)$',fontsize=fsl)
 axs[1].set_ylim(0,10000)
-
+# show figure
 plt.show()
