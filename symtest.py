958d8bb6d6c985a5fc58a0f0feeb5baf3ab350be
Hezy <hezy.amiel@gmail.com>
2019-05-29 18:13:50 +0300

    Update symtest.py

--- symtest.py
+++ symtest.py
@@ -6,7 +6,7 @@ author: hezy1a
 
 from sympy import init_printing, symbols, Integral, oo, exp, Eq, solve, Rational, latex
 from IPython.display import display
-init_printing(use_latex=True, latex_mode='equation*', fontsize='10', forecolor='White') 
+init_printing(use_latex=True, latex_mode='equation*', forecolor='White') 
 #remove "White" if your background is white
 
 x = symbols('x') 
