1d603694e605866e2045ebac8c9b0efd15dcb43e
Hezy <hezy.amiel@gmail.com>
2019-05-30 08:27:37 +0300

    Update symtest.py
    remarks cleanup

--- symtest.py
+++ symtest.py
@@ -16,12 +16,12 @@ gamma, sigma = symbols ('gamma sigma', positive=True)
 def lorentz(x, gamma):
     return 1/(gamma**2 + x**2)
 
-# finding the norm of lorentz
+# normnormalizing Lorentzian
 L_int = Integral(lorentz(x,gamma), (x, -oo, oo))
 L_norm = L_int.doit()
 display(Eq(L_int, L_norm ))
 
-# calculating the half width at half maximum (HWHM) for lorenz
+# calculating the half width at half maximum (HWHM) of a Lorenzian
 L_HWHM = Eq(lorentz(x,gamma),Rational(1,2))
 display(L_HWHM)
 display(solve(L_HWHM, x))
@@ -30,12 +30,12 @@ display(solve(L_HWHM, x))
 def gauss(x,sigma):
     return exp(-x**2/2/sigma**2)
 
-# finding the norm of gauss
+# normnormalizing Gaussian
 G_int = Integral(gauss(x,sigma), (x, -oo, oo))
 G_norm = G_int.doit()
 display(Eq(G_int, G_norm))
 
-# calculating the half width at half maximum (HWHM) for gauss
+# calculating the half width at half maximum (HWHM) of a Gaussian
 G_HWHM = Eq(gauss(x,sigma),Rational(1,2))
 display(G_HWHM)
 display(solve(G_HWHM, x))
