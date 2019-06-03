d55c38a8e0d2c7f27adb888a11f4fb423375cc70
Hezy <hezy.amiel@gmail.com>
2019-06-03 16:36:12 +0300

    Update pseudo-voigt.py

--- pseudo-voigt.py
+++ pseudo-voigt.py
@@ -41,25 +41,31 @@ def pseudo_voigt(x, w, n):
     # pseudo-voigt with max=1 and w=FWHM:
     return n * gauss(x, w) + (1-n) * lorentz(x,w)
 
-x = np.arange (-6, 6 , 0.01)
+x = np.arange (-10, 11 , 1e-2)
 #xfit = np.arange (-12.0, 12.0 , 0.01)
 
 w = 1.0
 yL = lorentz(x, w)
 yG = gauss(x, w)
 yPV = pseudo_voigt(x, w, 0.5)
-yV = voigt(x, 1, 1)
-           
+yV = voigt(x, .562855, .562855)
+
+popt, pcov = curve_fit(voigt, x, yPV, p0=(0.56285008, 0.56291417), sigma=None, bounds=(0,100))
+yV = voigt(x, *popt)          
 #popt_L, pcov_L = curve_fit(voigt, x, yL, p0=(1., 1e-9, 1.), sigma=None, bounds=(0,100))
 #yVL = voigt(xfit, *popt_L)
-
 
 #popt_G, pcov_G = curve_fit(voigt, x, yG, p0=(1., 2., 1e-9), sigma=None, bounds=(0,100))
 #yVG = voigt(xfit, *popt_G)
 
+
+wL, wG = np.mgrid[0.1:10.1:0.1, 0.1:10.1:0.1]
+yo = voigt(0, wL, wG)
+ 
+
 mpl.style.use('default')
 plt.close('all')
-fig, ax = plt.subplots(figsize=(14, 8))
+fig1, ax = plt.subplots(figsize=(14, 8))
 ax.grid(b=True, which='both', axis='both')
 ax.minorticks_on()
 ax.set_title("Gaussian and Lorentzian", fontsize=16)
@@ -72,6 +78,9 @@ ax.plot(x,yG, '-b', label='Gauss')
 ax.plot(x,yPV, '-m', label='Pseudo Voigt')
 ax.plot(x,yV, '-g', label='Voigt')
 ax.legend()
+
+fig2, ax2 = plt.subplots()
+
 plt.show()
 
 #print('Lorentzian:')
