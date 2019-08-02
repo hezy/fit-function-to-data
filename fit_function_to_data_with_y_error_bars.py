6503d7db923e863ba570ad1ac889ba4faff300f4
hezy <hezy.amiel@gmail.com>
2019-08-02 18:39:50 +0300

    Update fit_function_to_data_with_y_error_bars.py
    rewritten the code in a funcional style

--- fit_function_to_data_with_y_error_bars.py
+++ fit_function_to_data_with_y_error_bars.py
@@ -8,10 +8,63 @@ this script fits a defined function to a given data with y error bars
 
 
 import numpy as np
-from pandas import read_csv
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from decimal import Decimal
+
+
+def func(x, a1, a2, a3):
+    '''
+    a polynumial function of x
+    a1, a2, a3 are the coefficients
+    '''
+    return a1 * x**2 + a2*x + a3
+
+
+def fab_data(x_min, x_max, x_step, rand_size):
+    '''
+    fabricate data with function + random noise
+    use in case there's no csv file ready
+    ''' 
+    data = pd.DataFrame()
+    data['x'] = np.arange(x_min, x_max, x_step)
+    size = data.x.size
+    a = 3 * np.random.randn(3)
+    print('a = ' + str(a))
+    data['y'] = func(data.x, *a) + rand_size * data.x * np.random.randn(size)
+    data['dx'] = np.full((size), 0.2)
+    data['dy'] = data.x + 1    # y error bars increase with
+    print(data)
+    return data
+
+
+def fit_it (func, data):
+    '''
+    fit data with function
+    input: func, data
+    returns a tuple: [optimal parameters, estimated parameters errors]    
+    '''
+    popt, pcov = curve_fit(func, data.x, data.y, p0=None, sigma=data.dy)
+    perr = np.sqrt(np.diag(pcov))
+    return [popt, perr]
+
+
+def plot_it(data, fit_param):
+    '''
+    input: data (Pandas DataFarme)
+    output: a plot of the experimental results with the best fit 
+    '''
+    fig, ax = plt.subplots(figsize=(14, 8))
+    plt.errorbar(data.x, data.y, xerr=data.dx, yerr=data.dy, fmt='none', label='experiment')
+    plt.plot(data.x,func(data.x, *fit_param[0]))#, label='fit: a0=%5.3f, a1=%5.3f, a2=%5.3f' % tuple(fit_param[0]))
+    # arange figure
+    ax.grid(True)
+    ax.legend(loc='best')
+    ax.set_title('displacement vs time')
+    ax.set_xlabel('time (ms)')
+    ax.set_ylabel('displacment (mm)')
+    return plt.show()
 
 
 def round_to_error(x, Dx):
@@ -23,47 +76,27 @@ def round_to_error(x, Dx):
     return x_str + ' +/- ' + Dx_str
 
 
-# read data from csv file
-data = read_csv('sample01.csv', skiprows=0, header=0, sep=',')
-x = data.x
-dx = data.dx
-y = data.y
-dy = data.dy
-
-def func(x, a1, a2, a3):
+def print_fit_results(fit_param):
     '''
-    a polynumial function of x
-    a1, a2, a3 are the coefficients
+    printing the fit parameters with their error estimates
+    input: fit_param = [optimal parameters of fit, parameter estimated errors]
+    returns:
     '''
-    return a1 * x**2 + a2*x + a3
+    for i in range(0,3):
+        a = fit_param[0][i]
+        Da =  fit_param[1][i]
+        print (f'a{i} = ' + round_to_error(a,Da))
+        
+        
+# read data from csv file / fabricate new data
+# data = pd.read_csv('sample01.csv', skiprows=0, header=0, sep=',')
+DATA = fab_data(0, 20, 1, 3)
 
-"""
-# fabricate data with function + random noise
-# use in case there's no csv file ready
-x = np.arange(0.0, 20.0, 1.0)
-y = func(x, -1.5, 27.0, 12.0) + 0.8*x*np.random.randn(20)
-dx = np.full((20), 0.2)
-dy = x+1    # y error bars increase with x
-"""
+# fit it
+FIT_PARAM = fit_it(func,DATA)
 
-# fit data with function
-popt, pcov = curve_fit(func, x, y, p0=None, sigma=dy)
-perr = np.sqrt(np.diag(pcov))
+# plot it
+plot_it(DATA,FIT_PARAM)
 
-# create figure
-fig, ax = plt.subplots(figsize=(14, 8))
-plt.errorbar(x, y, xerr=dx, yerr=dy, fmt='none', label='experiment')
-plt.plot(x,func(x, *popt), label='fit: a0=%5.3f, a1=%5.3f, a2=%5.3f' % tuple(popt))
-
-# arange figure
-ax.grid(True)
-ax.legend(loc='best')
-ax.set_title('displacement vs time')
-ax.set_xlabel('time (ms)')
-ax.set_ylabel('displacment (mm)')
-
-plt.show()
-
-# printing the fit parameters with their error estimates
-for i in range(0,3):
-    print (f'a{i} = ' + round_to_error(popt[i],perr[i]) )
+# print fit results
+print_fit_results(FIT_PARAM)
