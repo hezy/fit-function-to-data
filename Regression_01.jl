05b3161e993add78640c3304ac1ae029720c7edb
Hezy Amiel <hezy.amiel@gmail.com>
2023-01-31 00:48:00 +0200

    started regression with julia

---
+++ Regression_01.jl
@@ -0,0 +1,15 @@
+using CSV, DataFrames, Plots, Measurements, Polynomials
+
+df = DataFrame(CSV.File("sample01.csv"))
+
+x = df[!, 1]
+Δx = df[!, 2]
+y = df[!, 3]
+Δy = df[!, 4]
+
+scatter(x .± Δx, y .± Δy,
+marker = 0, label = "experiment", legend = :topright)
+
+f = fit(x, y, 2)
+
+plot!(f, label = "fit")
